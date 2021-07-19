de1e3f675596d992a943217c8bc309a716cd73ad
Evgeny M <moskvitin199@gmail.com>
2021-07-19 20:50:00 +0300

    add react components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SiteLogo from '../SiteLogo/SiteLogo';
+
+export default function Footer() {
+	return (
+		<div>
+			<SiteLogo />
+		</div>
+	);
+}
---
+++ src/components/SiteLogo/SiteLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SiteLogo() {
+	return (
+		<div>
+			<span>netflix</span>
+			<span>roulette</span>
+		</div>
+	);
+}
